f0238531e502d1191b00f9eaa8c16ca3a7de4856
FollowNaim <naimxw@gmail.com>
2023-08-29 01:41:30 +0600

    minor fixes > authord by : naimxf@gmail.com

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = ({ toggle })=>{
   return(
     <div className="text-center mt-20">
     <div className="flex justify-center mx-auto">
-    <img className="w-[220px] h-[200px] flex justify-center align-center mt-[30%]" src="/images/images (3).jpeg" />
+    <img className="w-[230px] h-[200px] flex justify-center align-center mt-[5%]" src="/images/images3.png" />
     </div>
     <div className="flex flex-col justify-center align-center">
     <h1 className=" mx-auto text-3xl bold"> Dice Game </h1>
--- src/components/TotalScore.jsx
+++ src/components/TotalScore.jsx
@@ -1,6 +1,6 @@
 const TotalScore = ({score})=>{
   return(
-    <div className="flex justify-center items-center text-center flex-col mt-[5%] sm:mt-[30%]">
+    <div className="flex justify-center items-center text-center flex-col mt-[5%] sm:mt-[5%]">
     <span className="font-extrabold text-5xl">{score}</span>
     <p className="font-bold text-2xl">Total Score</p>
     </div>
